df9b0e13fc1d604d843cec96c919a4d073795f22
oooo91 <hyelneu29@gmail.com>
2024-01-11 18:12:01 +0900

    update, delete 시 validation modal 창 추가

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,20 @@
 
 /**모달 창 열기/닫기 */
-document.querySelector('.comment-button').addEventListener('click', openModal);
-
-function openModal() {
+function openReviewModal() {
   document.getElementById('comment-modal').style.display = 'block';
 }
 
-function closeModal1() {
-  document.getElementById('comment-modal').style.display = 'none';
-}
-
-function closeModal2() {
-  document.getElementById('userInfo-modal').style.display = 'none';
-}
-
-function nextModal() {
+function openValidationModal() {
   document.getElementById('comment-modal').style.display = 'none';
   document.getElementById('userInfo-modal').style.display = 'block';
+}
+
+function closeReviewModal() {
+  document.getElementById('comment-modal').style.display = 'none';
+}
+
+function closeValidationModal() {
+  document.getElementById('userInfo-modal').style.display = 'none';
 }
 
 /** 별점 */
@@ -35,10 +33,3 @@ rating_input.addEventListener('input', () => {
         }
     }
 });
-
-/**사용자 정보 저장 */
-function submitUserComment() {
-  document.getElementById('userInfo-modal').style.display = 'none';
-
-  //로컬 스토리지에 저장
-}
